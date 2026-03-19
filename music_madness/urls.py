6e37193dc13a5094dc215440341f87d9cc3b193e
Ash-39284 <ashroberts98@icloud.com>
2026-03-19 14:13:23 +0000

    Add heroku deployment

--- music_madness/urls.py
+++ music_madness/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from accounts import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('accounts/', index_views.accounts, name='index'),
     path('admin/', admin.site.urls),
 ]

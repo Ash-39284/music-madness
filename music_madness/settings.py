d316d5f7a481602958f35214de2d34eb4759ceef
Ash-39284 <ashroberts98@icloud.com>
2026-04-14 20:03:29 +0100

    Added all apps (accounts, albums, core, interactions)

--- music_madness/settings.py
+++ music_madness/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'albums',
+    'interactions',
+    'core',
 ]
 
 MIDDLEWARE = [

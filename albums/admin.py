ceef4588476f01633e3c6cf55c2e78c314b6153c
Ash-39284 <ashroberts98@icloud.com>
2026-04-22 20:58:31 +0100

    add album models to admin.py

--- albums/admin.py
+++ albums/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Genre, Album, Track
 
 # Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'genre', 'release_year', 'source', 'is_imported')
+    list_filter = ('genre', 'source', 'is_imported')
+    search_fields = ('title', 'artist')
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'album', 'track_number', 'duration_seconds')
+    search_fields = ('title', 'album__title')
+
+
+
+
+

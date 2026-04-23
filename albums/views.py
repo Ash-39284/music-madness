cd61d75b80f6b02cc3b1c9d6b5189b07c96e88de
Ash-39284 <ashroberts98@icloud.com>
2026-04-24 00:02:35 +0100

    add html to genre_detail.html and add links to each page in the navbar

--- albums/views.py
+++ albums/views.py
@@ -1,32 +1,34 @@
-from django.shortcuts import render
-from albums.models import Album
+from django.shortcuts import render, get_object_or_404
+from albums.models import Album, Genre
 
-# Create your views here.
+
 def home_view(request):
-    albums = Album.objects.filter(featured=True, ).exclude(cover_image_url='')[:3]
+    albums = Album.objects.filter(featured=True).exclude(cover_image_url='')[:3]
     return render(request, 'albums/home.html', {'albums': albums})
+
 
 def about_view(request):
     return render(request, 'albums/about.html')
+
 
 def genre_detail_view(request, slug):
     genre = get_object_or_404(Genre, slug=slug)
     search_query = request.GET.get('q', '')
     sort = request.GET.get('sort', 'artist')
- 
+
     albums = Album.objects.filter(genre=genre).exclude(cover_image_url='')
- 
+
     if search_query:
         albums = albums.filter(title__icontains=search_query) | \
                  Album.objects.filter(genre=genre).exclude(cover_image_url='').filter(artist__icontains=search_query)
- 
+
     if sort == 'title':
         albums = albums.order_by('title')
     else:
         albums = albums.order_by('artist')
- 
+
     album_count = Album.objects.filter(genre=genre).exclude(cover_image_url='').count()
- 
+
     return render(request, 'albums/genre_detail.html', {
         'genre': genre,
         'albums': albums,

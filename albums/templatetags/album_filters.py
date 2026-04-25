39732f568df034258bf9bda01a7e733698009a7e
Ash-39284 <ashroberts98@icloud.com>
2026-04-25 14:21:18 +0100

    Fix album description errors

--- albums/templatetags/album_filters.py
+++ albums/templatetags/album_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import re
 
 register = template.Library()
 
@@ -9,3 +10,17 @@ def duration(seconds):
     minutes = seconds // 60
     secs = seconds % 60
     return f"{minutes}:{secs:02d}"
+
+@register.filter
+def clean_description(value):
+    if not value:
+        return ''
+    """
+    Remove the "Read more on Last.fm" link and everything after it
+    """
+    cleaned = re.sub(r'<a href="[^"]*">Read more on Last\.fm</a>\.?', '', value)
+    """
+    Strip any remaining HTML tags
+    """
+    cleaned = re.sub(r'<[^>]+>', '', cleaned)
+    return cleaned.strip()

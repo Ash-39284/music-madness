7d12081474fe47e70db600798a8fc69f40a37905
Ash-39284 <ashroberts98@icloud.com>
2026-04-27 07:56:48 +0100

    Add reaction admin and comment admin to interactions admin.py

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Reaction, Comment
 
-# Register your models here.
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'album', 'reaction_type', 'created_at')
+    list_filter = ('reaction_type',)
+    search_fields = ('user__username', 'album__title')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'album', 'comment_text', 'parent_comment', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'album__title', 'comment_text')
+    readonly_fields = ('created_at', 'updated_at')

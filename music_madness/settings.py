897482c6520e1f87aa60b4297181e966a331ea85
Ash-39284 <ashroberts98@icloud.com>
2026-04-28 01:20:40 +0100

    Add SMTP configuration for password reset email

--- music_madness/settings.py
+++ music_madness/settings.py
@@ -74,7 +74,6 @@ ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -179,3 +178,17 @@ JAZZMIN_SETTINGS = {
         {"name": "Home", "url": "/", "new_window": True},
     ],
 }
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "ashroberts40@gmail.com"
+EMAIL_HOST_PASSWORD = "stgc orsj japh blzn"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 20

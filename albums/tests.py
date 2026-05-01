d50c5140c938a833e5c7b63cab04bfaa07688b1e
Ash-39284 <ashroberts98@icloud.com>
2026-05-01 19:15:32 +0100

    Add Album models test to albums app

--- albums/tests.py
+++ albums/tests.py
@@ -25,3 +25,54 @@ class GenreModelTest(TestCase):
         Genre.objects.create(name='Heavy Metal')
         with self.assertRaises(Exception):
             Genre.objects.create(name='Heavy Metal')
+
+class AlbumModelTest(TestCase):
+ 
+    def setUp(self):
+        self.genre = Genre.objects.create(name='Heavy Metal')
+        self.album = Album.objects.create(
+            title='Paranoid',
+            artist='Black Sabbath',
+            genre=self.genre,
+            release_year=1970,
+        )
+ 
+    def test_album_str(self):
+        self.assertEqual(str(self.album), 'Black Sabbath - Paranoid')
+ 
+    def test_like_count_no_reactions(self):
+        self.assertEqual(self.album.like_count(), 0)
+ 
+    def test_dislike_count_no_reactions(self):
+        self.assertEqual(self.album.dislike_count(), 0)
+ 
+    def test_like_count_with_reactions(self):
+        user = User.objects.create_user(username='testuser', password='pass')
+        Reaction.objects.create(user=user, album=self.album, reaction_type='like')
+        self.assertEqual(self.album.like_count(), 1)
+ 
+    def test_dislike_count_with_reactions(self):
+        user = User.objects.create_user(username='testuser', password='pass')
+        Reaction.objects.create(user=user, album=self.album, reaction_type='dislike')
+        self.assertEqual(self.album.dislike_count(), 1)
+ 
+    def test_comment_count_no_comments(self):
+        self.assertEqual(self.album.comment_count(), 0)
+ 
+    def test_comment_count_top_level_only(self):
+        user = User.objects.create_user(username='testuser', password='pass')
+        parent = Comment.objects.create(
+            user=user, album=self.album, comment_text='Top level comment'
+        )
+        # Reply should not be counted
+        Comment.objects.create(
+            user=user, album=self.album,
+            parent_comment=parent, comment_text='A reply'
+        )
+        self.assertEqual(self.album.comment_count(), 1)
+ 
+    def test_album_featured_default_false(self):
+        self.assertFalse(self.album.featured)
+ 
+    def test_album_is_imported_default_false(self):
+        self.assertFalse(self.album.is_imported)
